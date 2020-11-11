b1f7a75dd8f8bb82a33b5c0e3f9a7a931cfb59be
Lukey <ldipaola66@gmail.com>
2020-11-11 22:33:43 +1100

    add logout button

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { logout } from "../utils/logout";
 
 export default function Home() {
-    return (
-        <div>
-            <h1>Hello, User!</h1>
-        </div>
-    )
+  let history = useHistory();
+
+  async function handleClick(event) {
+    event.preventDefault();
+    try {
+      await logout();
+      history.push("/login");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Hello, User!</h1>
+      <Button type="submit" className="btn btn-default" onClick={handleClick}>
+        Logout
+      </Button>
+    </div>
+  );
 }

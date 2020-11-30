41aa2f0384541a59a3b693c5a4220499581f377d
Lukey <ldipaola66@gmail.com>
2020-11-30 23:48:23 +1100

    create list items

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import Button from "react-bootstrap/Button";
+import ListGroup from "react-bootstrap/ListGroup";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import { logout } from "../utils/logout";
 
 export default function Home() {
@@ -18,10 +22,26 @@ export default function Home() {
 
   return (
     <div>
-      <h1>Hello, User!</h1>
-      <Button type="submit" className="btn btn-default" onClick={handleClick}>
-        Logout
-      </Button>
+        <Row>
+          <Col>
+            <ListGroup variant="flush">
+              <ListGroup.Item style={{color: "#AFAFAF", border: "none"}}>Data Entry</ListGroup.Item>
+              <ListGroup.Item style={{color: "#AFAFAF", border: "none"}}>Reports</ListGroup.Item>
+              <ListGroup.Item style={{color: "#AFAFAF", border: "none"}}>TMU - Data Entry</ListGroup.Item>
+              <ListGroup.Item style={{color: "#AFAFAF", border: "none"}}>RNS - Data Entry</ListGroup.Item>
+              <ListGroup.Item style={{color: "#AFAFAF", border: "none"}}>Unit Email</ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col>
+            <Button
+              type="submit"
+              className="btn btn-default float-right"
+              onClick={handleClick}
+            >
+              Logout
+            </Button>
+          </Col>
+        </Row>
     </div>
   );
 }
